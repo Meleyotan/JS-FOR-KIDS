694515783b1cb90edc7c1272867f5bfe78002e9b
Meleyotan Oyeleke <meleyotanoyeleke@gmail.com>
2023-08-15 16:27:44 +0100

    Repository Initialized

---
+++ index.js
@@ -0,0 +1,9 @@
+//Write a function that prints a catface the number of times you want
+
+const printCatface = (howmany) => { 
+    for (i = 0; i < howmany; i++) { 
+        console.log("=.^^.=") 
+    } 
+}
+
+printCatface(10)
